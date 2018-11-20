23d8d63776dab2e8e2d79076bb75c80462afedb3
Jeremiah Carry Pandin <carrypandin@gmail.com>
2018-11-21 02:04:43 +0700

    [Setting Stock] Flag buat automatic unbooking

--- settings/application/modules/stocksetting/controllers/Stocksetting.php
+++ settings/application/modules/stocksetting/controllers/Stocksetting.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Stocksetting extends MY_Controller 
 {
-	private $_module = "SETTINGS_ROLE";
+	private $_module = "SETTINGS_STOCK_SETTING";
 	
     function __construct()
     {
