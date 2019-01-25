a6bd5c0193a6df36d36bd2f1bdc9043ecbe05fd2
Jeremiah Carry Pandin <carrypandin@gmail.com>
2019-01-25 16:47:50 +0700

    change credential gplus

--- one/application/config/googleplus.php
+++ one/application/config/googleplus.php
@@ -2,8 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['googleplus']['application_name'] = 'Jababeka - One';
-$config['googleplus']['client_id']        = '835388158460-93c2ja0ugfq6rjr1jc6d97uam1u796gd.apps.googleusercontent.com';
-$config['googleplus']['client_secret']    = 'r7PWVdpvOkXjqDTuVR_-Kc9t';
+// $config['googleplus']['client_id']        = '835388158460-93c2ja0ugfq6rjr1jc6d97uam1u796gd.apps.googleusercontent.com';
+// $config['googleplus']['client_secret']    = 'r7PWVdpvOkXjqDTuVR_-Kc9t';
+$config['googleplus']['client_id']        = '927731195010-i2epq11modjo49d3le0cm2h8e1dg3lo0.apps.googleusercontent.com';
+$config['googleplus']['client_secret']    = 'sDrZOAHgLdN8g8kNwHi-A65n';
 $config['googleplus']['redirect_uri']     = 'http://one.jababeka-stock.com/auth/callback';
 $config['googleplus']['api_key']          = '';
 $config['googleplus']['scopes']           = array();
