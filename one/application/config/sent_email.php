a702653c54b232a7614fafda6fc965f68b7fb3a9
Muhammad Redza Syahrial Effendi <mredzase@gmail.com>
2019-04-19 14:04:54 +0700

    update email remove ssl

--- one/application/config/sent_email.php
+++ one/application/config/sent_email.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config = array(
     'sent_email' => array(
         'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
-        'smtp_host' => 'ssl://smtp.gmail.com',
+        'smtp_host' => 'smtp.gmail.com',
         'smtp_port' => 587,
         'smtp_user' => 'jababeka.help@gmail.com',
         'smtp_pass' => 'jababeka1234',
